0d27f337d1bd3f70409ba654a53c4fe5595f569d
K.Siniakov <k.siniakov@gmail.com>
2022-05-11 14:24:52 +0300

    Stage 2

---
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -0,0 +1,15 @@
+package machine;
+
+import java.util.Scanner;
+
+public class CoffeeMachine {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write how many cups of coffee you will need:");
+        int b = scanner.nextInt();
+        System.out.println("For "+b+" cups of coffee you will need:\n" +
+                ""+ b * 200+" ml of water\n" +
+                ""+ b * 50+" ml of milk\n" +
+                ""+ b * 15+" g of coffee beans");
+    }
+}
