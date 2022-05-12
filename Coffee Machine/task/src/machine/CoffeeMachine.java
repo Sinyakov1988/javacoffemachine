68bb4be991a9f0b2ab98929685a83409b6e966e3
K.Siniakov <k.siniakov@gmail.com>
2022-05-12 10:29:43 +0300

    Stage 6 final

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -9,26 +9,11 @@ public class CoffeeMachine {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-//        System.out.println("Write how many ml of water the coffee machine has:");
-//        int water = scanner.nextInt();
-//
-//        System.out.println("Write how many ml of milk the coffee machine has:");
-//        int milk = scanner.nextInt();
-//
-//        System.out.println("Write how many grams of coffee beans the coffee machine has:");
-//        int beans = scanner.nextInt();
-//
-//        System.out.println("Write how many cups of coffee you will need:");
-//
-//        int cups = scanner.nextInt();
-
         Machine machine = new Machine(400, 540, 120, 9, 550);
         machine.sayMessageFromState(false);
 
         while (machine.getState() != CoffeeMachineState.WAITING_END) {
             machine.processCommand(scanner.next());
-            //scanner.nextLine();
-
         }
     }
 }
